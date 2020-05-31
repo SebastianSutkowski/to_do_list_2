b4009870e65f8d3c0ca0d6f7434b74d575aa79e2
Sebastian Sutkowski <sebastian.sutkowski1@gmail.com>
2020-05-31 17:08:58 +0200

    modify styles

--- main.js
+++ main.js
@@ -1,68 +1,85 @@
-const button = document.querySelector('.button_new')
-const new_task = document.querySelector('.new_task')
-let tasks_to_do = document.getElementById('field')
-let tasks_done = document.getElementById('field_done')
-const search_val = document.querySelector('input.search_task')
-let pasuje = []
-const to_do = new Tasks()
-const done = new Tasks()
+const button = document.querySelector(".button_new");
+const new_task = document.querySelector(".new_task");
+let tasks_to_do = document.getElementById("field");
+let tasks_done = document.getElementById("field_done");
+const search_val = document.querySelector("input.search_task");
+let pasuje = [];
+const to_do = new Tasks();
+const done = new Tasks();
 
 function search(e) {
-    const all_tasks = [...document.querySelectorAll('.task')]
-    all_tasks.forEach(element => {
-        element.classList.remove("wid")
-    });
-    value = e.target.value
-    pasuje = all_tasks.filter(element => !element.textContent.split(" ")[0].includes(value))
-    pasuje.forEach(element => {
-        element.classList.add("wid")
-    });
-    console.log('zadania')
-
+  const all_tasks = [...document.querySelectorAll(".task")];
+  all_tasks.forEach((element) => {
+    element.classList.remove("wid");
+  });
+  value = e.target.value;
+  pasuje = all_tasks.filter(
+    (element) => !element.textContent.split(" ")[0].includes(value)
+  );
+  pasuje.forEach((element) => {
+    element.classList.add("wid");
+  });
+  console.log("zadania");
 }
 
 function remove_task() {
-
-    this.parentNode.classList.add('done')
-    const all_tasks = document.querySelectorAll('.task')
-    all_tasks.forEach((element, index) => {
-        if (element.classList[1] == "done") {
-            done.add_new_task(to_do.getTasks()[index].getValue(), "-")
-            to_do.remove_task(index);
-            display()
-        }
-    });
-
+  this.parentNode.classList.add("done");
+  const all_tasks = document.querySelectorAll(".task");
+  all_tasks.forEach((element, index) => {
+    if (element.classList[1] == "done") {
+      done.add_new_task(to_do.getTasks()[index].getValue(), "-");
+      to_do.remove_task(index);
+      display();
+    }
+  });
+  checkToDoLength();
 }
 
 function display() {
-    while (tasks_to_do.firstChild) {
-        tasks_to_do.removeChild(tasks_to_do.firstChild);
-    }
-    to_do.getTasks().forEach(e => {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'task');
-        div.innerHTML = `<a>${e.getValue()}</a><button class="done">zrobione!</button>`
-        tasks_to_do.appendChild(div);
-        div.querySelector('button').addEventListener('click', remove_task)
-    });
-    while (tasks_done.firstChild) {
-        tasks_done.removeChild(tasks_done.firstChild);
-    }
-    done.getTasks().forEach(e => {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'task');
-        div.innerHTML = `${e.getValue()}`
-        tasks_done.appendChild(div);
-
-    });
-
+  while (tasks_to_do.firstChild) {
+    tasks_to_do.removeChild(tasks_to_do.firstChild);
+  }
+  to_do.getTasks().forEach((e) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "task");
+    div.innerHTML = `<a>${e.getValue()}</a><button class="done">zrobione!</button>`;
+    tasks_to_do.appendChild(div);
+    div.querySelector("button").addEventListener("click", remove_task);
+  });
+  while (tasks_done.firstChild) {
+    tasks_done.removeChild(tasks_done.firstChild);
+  }
+  done.getTasks().forEach((e) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "task");
+    div.innerHTML = `${e.getValue()}`;
+    tasks_done.appendChild(div);
+  });
 }
-
+function checkToDoLength() {
+  const tasksToDo = [...document.querySelector("#field").children];
+  const tasksDone = [...document.querySelector("#field_done").children];
+  //   console.log(tasksToDo.length);
+  if (tasksToDo.length === 0) {
+    document.querySelector("#field").style.opacity = "0";
+    console.log(tasksToDo.length);
+  } else {
+    document.querySelector("#field").style.opacity = "1";
+    console.log(tasksToDo.length);
+  }
+  if (tasksDone.length === 0) {
+    document.querySelector("#field_done").style.opacity = "0";
+    console.log(tasksDone.length);
+  } else {
+    document.querySelector("#field_done").style.opacity = "1";
+    console.log(tasksDone.length);
+  }
+}
 function addNewTask() {
-    const value = new_task.value;
-    to_do.add_new_task(value, "+");
-    display()
+  const value = new_task.value;
+  to_do.add_new_task(value, "+");
+  display();
+  checkToDoLength();
 }
-button.addEventListener('click', addNewTask)
-search_val.addEventListener('input', search)
+button.addEventListener("click", addNewTask);
+search_val.addEventListener("input", search);
